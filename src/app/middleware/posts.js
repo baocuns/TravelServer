e5501f67ab77acf2f6bdf158562d084df84a0527
Bao <baonv.23.student@fit.tdc.edu.vn>
2022-12-13 16:23:14 +0700

    update

--- src/app/middleware/posts.js
+++ src/app/middleware/posts.js
@@ -22,13 +22,13 @@ const PostsMiddleware = {
             })
         }
 
-        var AR = ''
-        if (address) {
-            AR = address
-        }
+        // var AR = ''
+        // if (address) {
+        //     AR = address
+        // }
 
         req.posts = {
-            address: AR,
+            address: address,
             content: content,
         }
         next()
